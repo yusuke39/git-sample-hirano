cf969e441649aa5a41b6b7ab0e45e8fb05839308
yusuke39 <hiranoyusuke39@gmail.com>
2019-08-01 15:09:17 +0900

    Carクラスを作成しました

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+public class Car {
+
+	private int speed;
+	private String bodyColor;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public String getBodyColor() {
+		return bodyColor;
+	}
+
+	public void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
+
+}
